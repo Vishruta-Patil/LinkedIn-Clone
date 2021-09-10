0161b771a942b08581fcc8bfd6234e517c847a81
Vishruta-Patil <vishrutapatil2106@gmail.com>
2021-09-10 18:12:31 +0530

    Redirecting to home page

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  //Details from firebase website
+    //Details from firebase website
   // api-key, auth-domain, projectId, storageBucket, appId, messagesenderId
 };
 
