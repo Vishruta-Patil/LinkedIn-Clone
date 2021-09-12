711c412c9c805aa98bc1f9c7f2c55cfd0147b285
Vishruta-Patil <vishrutapatil2106@gmail.com>
2021-09-13 03:42:47 +0530

    Backend implementation completed

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,17 @@
 import firebase from "firebase";
+require('dotenv').config();
 
 const firebaseConfig = {
-  
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
+
+
+console.log(process.env.REACT_APP_PROJECT_ID)
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
