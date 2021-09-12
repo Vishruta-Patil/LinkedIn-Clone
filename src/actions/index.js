7077eeef9e56a8169ae708e1c33730501b1cc71c
Vishruta-Patil <vishrutapatil2106@gmail.com>
2021-09-12 19:58:06 +0530

    Implememtaion: Upload post with video

--- src/actions/index.js
+++ src/actions/index.js
@@ -44,9 +44,9 @@ export function signOutAPI() {
 export function postArticleAPI(payload) {
   return (dispatch) => {
     if (payload.image !== "") {
-      const upload = storage.ref(`images/${payload.image.name}`).put(
-        payload.image
-      );
+      const upload = storage
+        .ref(`images/${payload.image.name}`)
+        .put(payload.image);
       upload.on(
         "state_changed",
         (snapshot) => {
@@ -61,20 +61,33 @@ export function postArticleAPI(payload) {
         (error) => console.log(error.code),
         async () => {
           const downloadURL = await upload.snapshot.ref.getDownloadURL();
-          db.collection("articles").add ({
-              actor: {
-                  description: payload.user.email,
-                  title:  payload.user.displayName,
-                  date: payload.timestamp,
-                  image: payload.user.photoURL,
-              },
-              video: payload.video,
-              shareImg: downloadURL,
-              comments: 0,
-              description: payload.description,
+          db.collection("articles").add({
+            actor: {
+              description: payload.user.email,
+              title: payload.user.displayName,
+              date: payload.timestamp,
+              image: payload.user.photoURL,
+            },
+            video: payload.video,
+            shareImg: downloadURL,
+            comments: 0,
+            description: payload.description,
           });
         }
       );
+    } else if (payload.video) {
+      db.collection("articles").add({
+        actor: {
+          description: payload.user.email,
+          title: payload.user.displayName,
+          date: payload.timestamp,
+          image: payload.user.photoURL,
+        },
+        video: payload.video,
+        shareImg: "",
+        comments: 0,
+        description: payload.description,
+      });
     }
   };
 }
