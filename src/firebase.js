18cd6057110c163f517e7cc3e3b47f6c9b1a1219
Vishruta-Patil <vishrutapatil2106@gmail.com>
2021-09-13 14:19:05 +0530

    Deploy firebase

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,10 @@ const firebaseConfig = {
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDER_ID,
+  appId: process.env.REACT_APP_APPID
 };
 
-
-console.log(process.env.REACT_APP_PROJECT_ID)
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
